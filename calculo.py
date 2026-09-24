b98c56b38228adeeb50ddc9e1e9cf174424a2ee9
Nicolas Ribeiro <nicaorib.1708@gmail.com>
2026-09-24 20:13:29 -0300

    primeira commit

---
+++ calculo.py
@@ -0,0 +1,12 @@
+def calcular_media(nota1,nota2)
+return( nota1 + nota2 ) /2
+
+print ("===Sistema de Notas de Aluno")
+n1= float (input ("Digite sua primeira nota: "))
+n2= float (input ("Digite sua segunda nota:  "))
+media = calcular_media(n1,n2)
+print (f "A média final é: {média:. 2f}")
+if media > 7.0:
+    print ("Status: APROVADO")
+    else:
+  print("Status: REPROVADO")
